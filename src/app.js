6c83779af0f124b1b4019d1bf1ba6e0beb4671f9
Vineet Sahu <sahuvince99@gmail.com>
2024-05-19 17:08:16 +0530

    add middlewares and conf cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
 import express from 'express'
 
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 export const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGING,
+    credentials: true
+}))
+
+app.use(express.json({limit:'15kb'}))
+app.use(express.urlencoded({extended:true,limit:'15kb'}))
+app.use(express.static('public'))
+app.use(cookieParser())
